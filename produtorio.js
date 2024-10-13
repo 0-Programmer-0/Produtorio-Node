d354fed5b519ade47ec156b920c6f95af26e3acc
unknown <jmoraes2003@hotmail.com>
2024-10-13 11:33:42 -0300

    ajustes ex API

--- produtorio.js
+++ produtorio.js
@@ -1,12 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+var resultado = 1;
+var x = 0;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Rota para capturar os parâmetros de rota
+app.get('/produtorio/:inf/:sup/:modo', (req, res) => {
+    // Captura os parâmetros 'a' e 'b' da rota
+    const inf = parseInt(req.params.inf);
+    const sup = parseInt(req.params.sup);
+    const modo = req.params.modo;
+
+    for (let i = inf; i <= sup; i++) {
+        x = i + (1 / i);
+        resultado *= x;
+    }
+
+    res.send(`O produtório com limite inferior ${inf} e limite superior ${sup} é: ${resultado}`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+    console.log(`Servidor rodando na porta http://localhost:${port}/produtorio/10/10/s`);
+});
